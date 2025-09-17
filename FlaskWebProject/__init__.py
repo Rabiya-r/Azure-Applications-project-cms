8edf6313bd385b02519e994a6c9517caf7c21137
Rabiya-r <91936927+Rabiya-r@users.noreply.github.com>
2025-09-17 21:15:32 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,7 +1,10 @@
 """
 The flask application package.
 """
+
 import logging
+from logging.handlers import RotatingFileHandler
+import os
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -10,10 +13,28 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+
+# ---------------- Logging Setup ----------------
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+
+file_handler = RotatingFileHandler('logs/cms_app.log', maxBytes=10240, backupCount=10)
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+
+# Set overall app logger level
+app.logger.setLevel(logging.INFO)
+app.logger.info('CMS Flask App startup')
+
+# ---------------- Flask Extensions ----------------
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
+# Import views after app is created
 import FlaskWebProject.views
+
