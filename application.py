1fc465cb78dd0ee3f8f3cdbc8b7f10c5ba16ebe1
Rabiya-r <91936927+Rabiya-r@users.noreply.github.com>
2025-09-18 11:45:28 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,12 @@
 from os import environ
 from FlaskWebProject import app
 
+
+
 # WSGI callable for Azure / Gunicorn
 application = app
+
+
 
 if __name__ == '__main__':
     host = environ.get('SERVER_HOST', '0.0.0.0')
