7420f6f2dbf6763ec7bd9419e4549b215aaaa349
Rabiya-r <91936927+Rabiya-r@users.noreply.github.com>
2025-09-18 11:20:50 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,8 +3,6 @@ from FlaskWebProject import app
 
 # WSGI callable for Azure / Gunicorn
 application = app
-
-# Optional: only run Flask dev server locally
 if __name__ == '__main__':
     host = environ.get('SERVER_HOST', '0.0.0.0')
     port = int(environ.get('SERVER_PORT', 8000))
