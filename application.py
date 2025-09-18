b454b600e4ecb6263f0a1b49e1314b80823ba1a7
Rabiya-r <91936927+Rabiya-r@users.noreply.github.com>
2025-09-18 12:29:31 +0530

    Update application.py

--- application.py
+++ application.py
@@ -11,3 +11,13 @@ if __name__ == '__main__':
     host = environ.get('SERVER_HOST', '0.0.0.0')
     port = int(environ.get('SERVER_PORT', 8000))
     application.run(host=host, port=port, debug=True)  
+
+from flask_login import LoginManager
+from models import User  # your User model
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))  # adjust depending on your DB
